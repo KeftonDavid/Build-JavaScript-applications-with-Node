da969a87c8e8435844243bb42ec2220aff177001
KeftonDavid <keftondavid@gmail.com>
2023-03-27 03:22:46 -0300

    Exercício: Trabalhar com o sistema de arquivos - Adição de comentários

--- node-essentials/node-files/index.js
+++ node-essentials/node-files/index.js
@@ -1,23 +1,29 @@
 const fs = require("fs").promises;
 
 async function findSalesFiles(folderName){
+        // array que recebe todos os itens na pasta atual
         let salesFiles = [];
 
         async function findFiles(folderName){
             const items = await fs.readdir(folderName, { withFileTypes: true });
-
+        // iterar sobre cada item encontrado
         for (item of items){
+            // se o item é uma pasta, procurar na pasta
             if(item.isDirectory()){
                 await findFiles(`${folderName}/${item.name}`);
             }
             else{
+                // se não é uma pasta, confirmar que é um sales.json
                 if(item.name === "sales.json"){
+                    // guardar o caminho do arquivo no array salesFiles
                     await salesFiles.push(`${folderName}/${item.name}`);
                 }
             }
         }
     }
+// encontrar os arquivos
 await findFiles(folderName);
+// retornar o array com o caminho dos arquivos
 return salesFiles;
 }
 async function main(){
